1fe81bf5ad5f76f99304b5d616f6032f198f9892
cfflm <fabbi.1935764@studenti.uniroma1.it>
2024-05-09 17:44:37 +0200

    Update account_account_tag.py

--- addons/account/models/account_account_tag.py
+++ addons/account/models/account_account_tag.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
-#io
 
+#io
 class AccountAccountTag(models.Model):
     _name = 'account.account.tag'
     _description = 'Account Tag'
